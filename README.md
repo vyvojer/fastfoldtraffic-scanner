2d52a4dd234cab2b7b89725df004829081afa864
vyvojer <vyvojer@gmail.com>
2017-11-29 18:33:16 +0100

    Add setup files

---
+++ README.md
@@ -0,0 +1,4 @@
+scanner
+======
+
+Python tool for scanning pokerstars
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='fftraffic-scanner',
+      version='0.1.0',
+      description='Python tool for scanning PokerStars traffic',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Utilities',
+      ],
+      url='https://bitbucket.org/vyvojer/fftraffic-scanner',
+      author='Alexey Londkevich',
+      author_email='vyvojer@gmail.com',
+      packages=['scanner'],
+      package_data={
+          'scanner': ['pokerstars_characters.dat', 'pokerstars_flags.dat']
+      },
+      zip_safe=False)
